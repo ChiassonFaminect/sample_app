2fe3bae5594329ae1f589908b2e0c28d1cb30e58
shiyason <yan@shiyason.com>
2015-11-12 04:10:28 +0900

    Refactor the About, Help, Home pages by using the layout file

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Japan travel tips"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Japan travel tips"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Japan travel tips"
   end
 
 end
